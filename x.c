f7cb7ce927a9ede2767827c7f63c2f0d77368adc
Aubry Mangold <aubry.mangold@outlook.com>
2018-10-29 20:40:27 +0100

    :sparkles: Patch clipboard to set PRIMARY buffer

--- x.c
+++ x.c
@@ -655,6 +655,8 @@ setsel(char *str, Time t)
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
 	if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
 		selclear();
+
+	clipcopy(NULL);
 }
 
 void
